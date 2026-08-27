37b780029a313e942e588b2389d9b1fb0a01ec35
kiri_main <hw23a073@oecu.jp>
2026-08-28 02:00:55 +0900

    繰り返し、再生停止、戻る、次へボタン追加

---
+++ MusicsPage/musicFunction.js
@@ -0,0 +1,108 @@
+//ループボタン
+const loopButton = document.getElementById("loopButton");
+let isLoop = false;
+loopButton.addEventListener("click", (e) =>{
+    if(e.target.id === "loopButton" && !isLoop)
+    {
+        audio.loop = true;
+        isLoop = true;
+
+        e.target.src = "../Images/loopOn.png";
+    }
+    else if(e.target.id === "loopButton" && isLoop)
+    {
+        audio.loop = false;
+        isLoop = false;
+
+        e.target.src = "../Images/loopOff.png";
+    }
+});
+
+//再生停止
+const playButton = document.getElementById("playButton");
+playButton.addEventListener("click", (e) =>{
+    if(e.target.id === "playButton" && audio.paused)
+    {
+        audio.play();
+    }
+    else if(e.target.id === "playButton" && !audio.paused)
+    {
+        audio.pause();
+    }
+});
+
+//再生、停止アイコン切り替え
+audio.addEventListener("play", () => {
+    playButton.src = "../Images/stop.png";
+});
+
+audio.addEventListener("pause", () => {
+    playButton.src = "../Images/play.png";
+});
+
+//戻る、次ボタン
+const nextButton = document.getElementById("nextButton");
+const backButton = document.getElementById("backButton");
+
+nextButton.addEventListener("click", (e) =>{
+    if(e.target.id === "nextButton" && isPlayList)
+    {
+        //リストにまだ曲がある場合
+        if(listNum < musicList.length - 1)
+        {
+            audio.src = URL.createObjectURL(musicList[listNum + 1]);
+            listNum++;
+            musicName = musicList[listNum].name;
+
+            //再生中の曲をハイライト
+            if(currentMusic !== null)
+            {
+                currentMusic.classList.remove("playing");
+            }
+            currentMusic = playList.children[listNum];
+            currentMusic.classList.add("playing");
+
+            audio.play();
+        }
+        else
+        {
+            //ハイライトを削除
+            if(currentMusic !== null)
+            {
+                currentMusic.classList.remove("playing");
+            }
+        }
+    }
+});
+
+backButton.addEventListener("click", (e) =>{
+    if(e.target.id === "backButton" && isPlayList)
+    {
+        //リストにまだ曲がある場合
+        if(listNum > 0)
+        {
+            listNum--;
+            audio.src = URL.createObjectURL(musicList[listNum]);
+            
+            musicName = musicList[listNum].name;
+
+            //再生中の曲をハイライト
+            if(currentMusic !== null)
+            {
+                currentMusic.classList.remove("playing");
+            }
+            currentMusic = playList.children[listNum];
+            currentMusic.classList.add("playing");
+
+            audio.play();
+        }
+        else
+        {
+            //ハイライトを削除
+            if(currentMusic !== null)
+            {
+                currentMusic.classList.remove("playing");
+            }
+        }
+    }
+});
